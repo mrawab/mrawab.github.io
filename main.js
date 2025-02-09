d97dc4b6a3bf503b642c1a3a348813ca396a9925
Awab Azhari <awab.azharii@gmail.com>
2025-02-09 13:58:43 -0800

    Update main.js

--- main.js
+++ main.js
@@ -1,43 +1,43 @@
+// variables
+//const hoverOne = document.getElementById("project-one");
+//const hoverTwo = document.getElementById("project-two");
+const mobileHeader = document.getElementById("mobile-header");
+const overlay = document.getElementById("overlay");
+
 //projects
-function display_project_website() {
-  const hover = document.querySelector(".project-one");
-  hover.style.opacity=1;
-  hover.style.transform='scale(1.2)';
-  hover.style.visibility='visible';
-  hover.style.transition='transform 0.5s ease-in-out , opacity 0.5s ease-in-out, visibility 0.5s';
+/*function display_project_website() {
+  hoverOne.style.opacity=1;
+  hoverOne.style.transform='scale(1.2)';
+  hoverOne.style.visibility='visible';
+  hoverOne.style.transition='transform 0.5s ease-in-out , opacity 0.5s ease-in-out, visibility 0.5s';
 }
 function hide_project_website() {
-  const hover = document.querySelector(".project-one");
-  hover.style.opacity=0;
-  hover.style.transform='scale(1)';
-  hover.style.visibility='hidden';
-}
-function display_project_telegram_bot() {
-  const hover = document.querySelector(".project-two");
-  hover.style.opacity=1;
-  hover.style.transform='scale(1.2)';
-  hover.style.visibility='visible';
-  hover.style.transition='transform 0.5s ease-in-out , opacity 0.5s ease-in-out, visibility 0.5s';
-}
-function hide_project_telegram_bot() {
-  const hover = document.querySelector(".project-two");
-  hover.style.opacity=0;
-  hover.style.transform='scale(1)';
-  hover.style.visibility='hidden';
+  hoverOne.style.opacity=0;
+  hoverOne.style.transform='scale(1)';
+  hoverOne.style.visibility='hidden';
 }
 
-//nav bar
-function hide_nav_bar() {
-  const mobileHeader = document.getElementById("mobile-header");
-  const overlay = document.getElementById("overlay");
- // mobileHeader.style.display = "none"; 
-  mobileHeader.classList.remove("show");
-  overlay.style.display = "none";
+function display_project_telegram_bot() {
+  hoverTwo.style.opacity=1;
+  hoverTwo.style.transform='scale(1.2)';
+  hoverTwo.style.visibility='visible';
+  hoverTwo.style.transition='transform 0.5s ease-in-out , opacity 0.5s ease-in-out, visibility 0.5s';
 }
+function hide_project_telegram_bot() {
+  hoverTwo.style.opacity=0;
+  hoverTwo.style.transform='scale(1)';
+  hoverTwo.style.visibility='hidden';
+}*/
+
+//nav bar
 function show_nav_bar() {
-  const mobileHeader = document.getElementById("mobile-header");
-  const overlay = document.getElementById("overlay");
   //mobileHeader.style.display = "flex";
   mobileHeader.classList.add("show");
   overlay.style.display = "block";
+}
+
+function hide_nav_bar() {
+ // mobileHeader.style.display = "none"; 
+  mobileHeader.classList.remove("show");
+  overlay.style.display = "none";
 }
