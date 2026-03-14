69954412867ccdaf2127a6c05bc714cf97297aa5
Awab Azhari <awab.azharii@gmail.com>
2026-03-14 06:38:30 +0200

    Update main.js

--- javascript/main.js
+++ javascript/main.js
@@ -3,6 +3,11 @@ const toggle = document.querySelector(".mobile-menu-toggle");
 const closeBtn = document.querySelector(".mobile-menu-close");
 const header = document.querySelector(".mobile-header");
 const overlay = document.querySelector(".overlay");
+const homeLink = document.querySelector("#home");
+const aboutLink = document.querySelector("#about-button");
+const projectsLink = document.querySelector("#project-button");
+const contactLink = document.querySelector("contact-button");
+// const mobileMenuLinks = document.querySelectorAll(".mobile-menu a");
 // const mobileWarning = document.querySelector("#mobile-warning");
 
 document.addEventListener("DOMContentLoaded", () => {
@@ -20,6 +25,22 @@ function isMobile() {
   );
 }
 
+// const lenis = new Lenis({
+//   duration: 0.5,
+//   smooth: true,
+// });
+
+// function raf(time) {
+//   lenis.raf(time);
+//   requestAnimationFrame(raf);
+// }
+// requestAnimationFrame(raf);
+
+overlay.addEventListener("click", () => {
+  header.classList.remove("open");
+  overlay.classList.remove("open");
+});
+
 toggle.addEventListener("click", () => {
   header.classList.add("open");
   overlay.classList.add("open");
@@ -30,7 +51,22 @@ closeBtn.addEventListener("click", () => {
   overlay.classList.remove("open");
 });
 
-overlay.addEventListener("click", () => {
+homeLink.addEventListener("click", () => {
+  header.classList.remove("open");
+  overlay.classList.remove("open");
+});
+
+aboutLink.addEventListener("click", () => {
+  header.classList.remove("open");
+  overlay.classList.remove("open");
+});
+
+projectsLink.addEventListener("click", () => {
+  header.classList.remove("open");
+  overlay.classList.remove("open");
+});
+
+contactLink.addEventListener("click", () => {
   header.classList.remove("open");
   overlay.classList.remove("open");
 });
